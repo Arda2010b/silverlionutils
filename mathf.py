a2e09a1c8df31dc844e28d923b8aaef3b0ab522c
MrPilotMan <60583177+Arda2010b@users.noreply.github.com>
2023-01-12 22:05:56 +0000

    version: public-alpha 1
    Adds: add, subtract, multiply, divide, squared (power of 2) and cubed (power of 3)

---
+++ mathf.py
@@ -0,0 +1,23 @@
+def add(x, y, ans):
+    ans = x + y
+    return(ans)
+
+def subtract(x, y, ans):
+    ans = x - y
+    return(ans)
+
+def multiply(x, y, ans):
+    ans = x * y
+    return(ans)
+
+def divide(x, y, ans):
+    ans = x /y
+    return(ans)
+
+def squared(x, ans):
+    ans = x * x
+    return(ans)
+
+def cubed(x, ans):
+    ans = x * x * x
+    return(ans)
