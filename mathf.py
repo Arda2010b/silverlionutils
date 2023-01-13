536ea25ecf81d83a5e33e162ee02c30c5034fdc4
MrPilotMan <60583177+Arda2010b@users.noreply.github.com>
2023-01-13 15:50:53 +0000

    Beta release
    Version: Beta 1.0

--- mathf.py
+++ mathf.py
@@ -1,23 +1,41 @@
-def add(x, y, ans):
-    ans = x + y
-    return(ans)
+import math #imports math to allow you to do sqroot and more functions in the future (or present as i wont change this lol)
 
-def subtract(x, y, ans):
-    ans = x - y
-    return(ans)
+def add(x, y):
+    return(x + y)
 
-def multiply(x, y, ans):
-    ans = x * y
-    return(ans)
+def subtract(x, y):
+    return(x - y)
 
-def divide(x, y, ans):
-    ans = x /y
-    return(ans)
+def multiply(x, y):
+    return(x * y)
 
-def squared(x, ans):
-    ans = x * x
-    return(ans)
+def divide(x, y):
+    return(x / y)
 
-def cubed(x, ans):
-    ans = x * x * x
-    return(ans)
+def sqrt(x): 
+    return(math.sqrt(x))
+
+def powerof(x, y):
+    return(x ** y)
+
+def modulo(x, y):
+    return(x % y)
+
+def average(x, y):
+    return((x + y) / 2)
+
+def sin(x, ans):
+    return(math.sin(x))
+
+def cosine(x):
+    return(math.cos(x))
+
+def tangent(x):
+    return(math.tan(x))
+
+def log(x):
+    return(math.log(x))
+
+def pythagorus(x, y):
+    return math.sqrt(x**2 + y**2)
+
