7d0c01d7a13a6b4e54f88aaa4f0c1212743e006b
MrPilotMan <60583177+Arda2010b@users.noreply.github.com>
2023-01-17 15:02:41 +0000

    Update silverlion.py

--- silverlion.py
+++ silverlion.py
@@ -1,43 +1,57 @@
 import math
 
-def mathf(x, y=None):
+class mathf:
+    @staticmethod
     def add(x, y):
-        return(x + y)
+        return x + y
 
+    @staticmethod
     def subtract(x, y):
-        return(x - y)
+        return x - y
 
+    @staticmethod
     def multiply(x, y):
-        return(x * y)
+        return x * y
 
+    @staticmethod
     def divide(x, y):
-        return(x / y)
+        return x / y
 
+    @staticmethod
     def sqrt(x): 
-        return(math.sqrt(x))
+        return math.sqrt(x)
 
+    @staticmethod
     def powerof(x, y):
-        return(x ** y)
+        return x ** y
 
+    @staticmethod
     def modulo(x, y):
-        return(x % y)
+        return x % y
 
+    @staticmethod
     def average(x, y):
-        return((x + y) / 2)
+        return (x + y) / 2
 
-    def sin(x, ans):
-        return(math.sin(x))
+    @staticmethod
+    def sin(x):
+        return math.sin(x)
 
+    @staticmethod
     def cosine(x):
-        return(math.cos(x))
+        return math.cos(x)
 
+    @staticmethod
     def tangent(x):
-        return(math.tan(x))
+        return math.tan(x)
 
+    @staticmethod
     def log(x):
-        return(math.log(x))
+        return math.log(x)
 
+    @staticmethod
     def pythagorus(x, y):
         return math.sqrt(x**2 + y**2)
-    
-    return locals()[x](y)
+
+# usage 
+mathf.add(2,3)
