0538a8f0ffb9512bf50e8857122b7d1831da4485
MrPilotMan <60583177+Arda2010b@users.noreply.github.com>
2023-01-17 14:48:16 +0000

    complete overhaul
    made it so i can add more function no tjust math but if ou want math you can use jsut math by using from silverlion import mathf

---
+++ silverlion.py
@@ -0,0 +1,43 @@
+import math
+
+def mathf(x, y=None):
+    def add(x, y):
+        return(x + y)
+
+    def subtract(x, y):
+        return(x - y)
+
+    def multiply(x, y):
+        return(x * y)
+
+    def divide(x, y):
+        return(x / y)
+
+    def sqrt(x): 
+        return(math.sqrt(x))
+
+    def powerof(x, y):
+        return(x ** y)
+
+    def modulo(x, y):
+        return(x % y)
+
+    def average(x, y):
+        return((x + y) / 2)
+
+    def sin(x, ans):
+        return(math.sin(x))
+
+    def cosine(x):
+        return(math.cos(x))
+
+    def tangent(x):
+        return(math.tan(x))
+
+    def log(x):
+        return(math.log(x))
+
+    def pythagorus(x, y):
+        return math.sqrt(x**2 + y**2)
+    
+    return locals()[x](y)
